ceed68d1b684e3c3468390434598fef9ffb9c327
LiliyaPtashkina <efekta9@gmail.com>
2022-06-25 13:12:03 +0300

    upd task 3

--- lesson_1/task_3.py
+++ lesson_1/task_3.py
@@ -1,19 +1,8 @@
-def factorial(n: int):
-    a = 1
-    for i in range(1, n+1):
-        a *=i
-    return a
-
-
 def zeros(n):
-    list_of = list(str(factorial(n)))
     count = 0
-    for i in list_of[::-1]:
-        if i == '0':
-            count += 1
-        else:
-            break
-    print(count)
+    while n >= 5:
+        n //= 5
+        count += n
     return count
 
 
