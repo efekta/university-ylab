9e43bcf479e506d59b514a0015c48d9b5877e44a
LiliyaPtashkina <efekta9@gmail.com>
2022-06-25 12:33:36 +0300

    ready task 2

---
+++ lesson_1/task_2.py
@@ -0,0 +1,10 @@
+import ipaddress
+from ipaddress import IPv4Address
+
+def int32_to_ip(int32):
+    return str(ipaddress.IPv4Address(int32))
+
+
+assert int32_to_ip(2154959208) == "128.114.17.104"
+assert int32_to_ip(0) == "0.0.0.0"
+assert int32_to_ip(2149583361) == "128.32.10.1"
